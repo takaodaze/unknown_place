2ba734eb29bc8bd401456a439ea626a8231ee327
takaodaze <gin2798@gmail.com>
2022-01-02 19:19:47 +0900

    install emotion

---
+++ src/components/TextBoard.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+export const TextBoard = () => {
+  return (
+    <div>
+      <TextArea />
+    </div>
+  );
+};
+
+const TextArea = styled.textarea({});
